0e917c3bbac644fd18fbb466bf84b15ecc31bef3
yurikajoshi <yukayuva@gmail.com>
2024-03-08 21:10:10 +0000

    added argument for Scaling content and opacity named contentScale and alpha with values set as ContentScale.Crop and 0.5F respectively in Image composable.

--- HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.layout.ContentScale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -69,7 +70,9 @@ fun GreetingImage(message: String, from: String, modifier:Modifier = Modifier){
     Box(modifier){
         Image(
             painter = image,
-            contentDescription = null
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
         )
         GreetingText(
             message = message,
