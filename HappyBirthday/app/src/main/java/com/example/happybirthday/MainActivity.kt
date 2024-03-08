62821c93e0f68008fb0da95ebb66adf7ed996279
yurikajoshi <yukayuva@gmail.com>
2024-03-08 00:50:06 +0000

    passed modifier parameter for Column composable.

--- HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(message: String, from: String, modifier:Modifier = Modifier){
-    Column {
+    Column(modifier = modifier) {
         Text(
             text = message,
             fontSize = 100.sp,
