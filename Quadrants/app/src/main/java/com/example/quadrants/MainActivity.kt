baaaa2381cd07eb3bc8239358468752909396bd5
yurikajoshi <yukayuva@gmail.com>
2024-03-13 23:44:57 +0000

    removed function MainPage() and replaced it with a new composable function CardMessage() based on its functionality.

--- Quadrants/app/src/main/java/com/example/quadrants/MainActivity.kt
+++ Quadrants/app/src/main/java/com/example/quadrants/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainPage()
+
                 }
             }
         }
@@ -30,7 +30,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainPage(modifier: Modifier = Modifier) {
+fun CardMessage(){
 
 }
 
