f38269c7ca66fd65c0db315136551ab4591e1136
yurikajoshi <yukayuva@gmail.com>
2024-03-14 00:35:35 +0000

    added arguments for padding in first Text composable and ALignment for text in second Text composable.

--- Quadrants/app/src/main/java/com/example/quadrants/MainActivity.kt
+++ Quadrants/app/src/main/java/com/example/quadrants/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -12,6 +13,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.quadrants.ui.theme.QuadrantsTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.style.TextAlign
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,11 +36,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun CardMessage(topic: String, description: String, modifier: Modifier = Modifier){
     Text(
-        text = topic
-
+        text = topic,
+        modifier = Modifier.padding(bottom = 16.dp)
     )
+
     Text(
-        text = description
+        text = description,
+        textAlign = TextAlign.Justify
     )
 }
 
