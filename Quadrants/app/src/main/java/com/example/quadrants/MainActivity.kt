3d2f9fcbfe29a9ce420f23220e66fd9beec8b746
yurikajoshi <yukayuva@gmail.com>
2024-03-14 15:58:59 +0000

    added argument horizontalAlignment for Column composable with a value passed such that elements are horizontally centered.

--- Quadrants/app/src/main/java/com/example/quadrants/MainActivity.kt
+++ Quadrants/app/src/main/java/com/example/quadrants/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -43,7 +44,8 @@ fun CardMessage(topic: String, description: String, modifier: Modifier = Modifie
     Column (modifier = modifier
                 .padding(16.dp)
                 .fillMaxHeight(),
-            verticalArrangement = Arrangement.Center
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
 
     ){
         Text(
