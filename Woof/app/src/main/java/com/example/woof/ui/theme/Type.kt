fff0dee627e0c43ad64179185c3a7bcfa1d5fa8d
yurikajoshi01 <joshiyurika2001@gmail.com>
2024-03-21 02:08:22 +0000

    added four attributes named, displayLarge, displayMedium, labelSmall, bodyLarge setting it equal to TextStyle and passing three attributes, fontFamily, fintWeight, fontSize with various size,weight and font style in val Typography.

--- Woof/app/src/main/java/com/example/woof/ui/theme/Type.kt
+++ Woof/app/src/main/java/com/example/woof/ui/theme/Type.kt
@@ -20,10 +20,8 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import com.example.woof.R
 import androidx.compose.ui.text.font.FontWeight
-
-// Set of Material typography styles to start with
-val Typography = Typography(
-)
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
 
 val AbrilFatface = FontFamily(
     Font(R.font.abril_fatface_regular)
@@ -33,3 +31,28 @@ val Montserrat = FontFamily(
     Font(R.font.montserrat_regular),
     Font(R.font.montserrat_bold, FontWeight.Bold)
 )
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = AbrilFatface,
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    )
+)
+
