a6c5d6f7f1c25d2f967e69b4c2fc28928b14fc81
yurikajoshi01 <joshiyurika2001@gmail.com>
2024-03-21 18:26:36 +0000

    passed item.second by calling onNextButtonClicked in lambda body of onClick in SelectQuantityButton present in row of StartOrderScreen function, to pass number of cupcakes.

--- Cupcake/app/src/main/java/com/example/cupcake/ui/StartOrderScreen.kt
+++ Cupcake/app/src/main/java/com/example/cupcake/ui/StartOrderScreen.kt
@@ -85,7 +85,7 @@ fun StartOrderScreen(
                 quantityOptions.forEach { item ->
                     SelectQuantityButton(
                         labelResourceId = item.first,
-                        onClick = {}
+                        onClick = { onNextButtonClicked(item.second)}
                     )
                 }
             }
