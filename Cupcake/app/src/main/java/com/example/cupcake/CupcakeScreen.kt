a492c592d108280a91479b68b397c1e8aba18ffc
yurikajoshi01 <joshiyurika2001@gmail.com>
2024-03-21 20:32:18 +0000

    called startActivity() method of context in function shareOrder, creating activity from intent by calling createChooser() class and passed intent and a string resource argument.

--- Cupcake/app/src/main/java/com/example/cupcake/CupcakeScreen.kt
+++ Cupcake/app/src/main/java/com/example/cupcake/CupcakeScreen.kt
@@ -152,12 +152,23 @@ fun CupcakeApp(
     }
 }
 
+/**
+ *Creates an intent for sharinf the order details.
+ */
+
 private fun shareOrder(context: Context, subject: String, summary: String) {
     val intent = Intent(Intent.ACTION_SEND).apply {
         type = "text/plain"
         putExtra(Intent.EXTRA_SUBJECT, subject)
         putExtra(Intent.EXTRA_TEXT, summary)
     }
+    context.startActivity(
+        Intent.createChooser(
+            intent,
+            context.getString(R.string.new_cupcake_order)
+        )
+    )
+
 }
 
 private fun cancelOrderAndNavigateToStart( viewModel: OrderViewModel,
