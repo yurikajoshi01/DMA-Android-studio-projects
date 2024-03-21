f5a01b887219cccd2dc9324a1f7e91dfeb32112f
yurikajoshi01 <joshiyurika2001@gmail.com>
2024-03-21 19:28:31 +0000

    passed empty lambda for onCancelButtonCLicked , and added paramters in subject and summary passed in onSendButtonClicked for composable of route CupcakeScreen.Summary.name

--- Cupcake/app/src/main/java/com/example/cupcake/CupcakeScreen.kt
+++ Cupcake/app/src/main/java/com/example/cupcake/CupcakeScreen.kt
@@ -140,6 +140,10 @@ fun CupcakeApp(
             composable(route = CupcakeScreen.Summary.name) {
                 OrderSummaryScreen(
                     orderUiState = uiState,
+                    onCancelButtonClicked = {},
+                    onSendButtonClicked = { subject: String, summary: String ->
+
+                    },
                     modifier = Modifier.fillMaxHeight()
                 )
             }
