437ff0b9559bf9e07372495d089f42c2b5582518
yurikajoshi01 <joshiyurika2001@gmail.com>
2024-03-21 01:10:51 +0000

    changed the dynamic color value to true in Theme.kt to check dynamic theming; checked by changing wallpapers.

--- Woof/app/src/main/java/com/example/woof/ui/theme/Theme.kt
+++ Woof/app/src/main/java/com/example/woof/ui/theme/Theme.kt
@@ -86,7 +86,7 @@ private val DarkColors = darkColorScheme(
 fun WoofTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = false,
+    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
