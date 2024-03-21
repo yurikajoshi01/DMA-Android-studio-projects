e879f145a96e0fb8f815f781ac0698138bf8a8c4
yurikajoshi01 <joshiyurika2001@gmail.com>
2024-03-21 12:26:55 +0000

    added a Column composable in Card() present in AffirmationCard() to arrange the cards vertically allowing image to be placed above text.

--- Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
+++ Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.affirmations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -42,7 +43,9 @@ fun AffirmationsApp(){
 @Composable
 fun AffirmationCard(affirmation: Affirmation, modifier: Modifier){
     Card(modifier = modifier){
+        Column{
 
+        }
     }
 }
 
