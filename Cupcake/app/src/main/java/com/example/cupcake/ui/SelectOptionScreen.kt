2201e54e4f10b100d3f26fd459b90357e1492b8d
yurikajoshi01 <joshiyurika2001@gmail.com>
2024-03-21 18:31:03 +0000

    added onCancelButtonClicked parameter of type () -> Unit with a default value {}. in SelectOptionScreen function in Se;ectoptionScreen.kt

--- Cupcake/app/src/main/java/com/example/cupcake/ui/SelectOptionScreen.kt
+++ Cupcake/app/src/main/java/com/example/cupcake/ui/SelectOptionScreen.kt
@@ -51,6 +51,7 @@ fun SelectOptionScreen(
     subtotal: String,
     options: List<String>,
     onSelectionChanged: (String) -> Unit = {},
+    onCancelButtonClicked: () -> Unit = {},
     modifier: Modifier = Modifier
 ){
     var selectedValue by rememberSaveable { mutableStateOf("") }
