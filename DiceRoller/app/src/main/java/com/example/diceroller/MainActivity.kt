8226bd59d987f81db486f8372f5bc8c7dcd4b2f4
yurikajoshi01 <joshiyurika2001@gmail.com>
2024-03-21 21:53:46 +0000

    created an immutable variable imageResource such that the variable is set to a when expression accepting resulr variable and setting possible results to its drawable (Works as a loop).

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -60,6 +60,14 @@ fun DiceRollerApp() {
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
     var result by remember { mutableStateOf(1) }
+    val imageResource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
