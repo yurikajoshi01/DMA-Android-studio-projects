51ec6e67bcdc814c1f30267c66c7591c177067d2
yurikajoshi01 <joshiyurika2001@gmail.com>
2024-03-21 16:19:57 +0000

    called SelectOptionScreen() inside the second composable's lambda function.

--- Cupcake/app/src/main/java/com/example/cupcake/CupcakeScreen.kt
+++ Cupcake/app/src/main/java/com/example/cupcake/CupcakeScreen.kt
@@ -41,6 +41,7 @@ import androidx.compose.ui.res.dimensionResource
 import com.example.cupcake.ui.StartOrderScreen
 import com.example.cupcake.data.DataSource
 import androidx.compose.ui.platform.LocalContext
+import com.example.cupcake.ui.SelectOptionScreen
 
 
 enum class CupcakeScreen(){
@@ -108,6 +109,7 @@ fun CupcakeApp(
 
             composable(route = CupcakeScreen.Flavor.name){
                 val context = LocalContext.current
+                SelectOptionScreen()
             }
         }
     }
