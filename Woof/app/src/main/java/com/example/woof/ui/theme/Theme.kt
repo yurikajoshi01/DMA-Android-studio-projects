bae5c2fc37bdf3a5d13c89ceb08c74cfb9b7da70
yurikajoshi01 <joshiyurika2001@gmail.com>
2024-03-21 01:12:03 +0000

    set the value of dynamicColor parameter to false back again , as the previous change was just a test.

--- Woof/app/src/main/java/com/example/woof/ui/theme/Theme.kt
+++ Woof/app/src/main/java/com/example/woof/ui/theme/Theme.kt
@@ -86,7 +86,7 @@ private val DarkColors = darkColorScheme(
 fun WoofTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
