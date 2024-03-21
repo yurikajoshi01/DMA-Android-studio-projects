b01935e1bc0d6b3538a11311865b2d63d275f409
yurikajoshi01 <joshiyurika2001@gmail.com>
2024-03-21 02:51:06 +0000

    added a verticalAlignment parameter in Row() to set the alignment equally for text and image.

--- Woof/app/src/main/java/com/example/woof/MainActivity.kt
+++ Woof/app/src/main/java/com/example/woof/MainActivity.kt
@@ -48,6 +48,7 @@ import com.example.woof.data.dogs
 import com.example.woof.ui.theme.WoofTheme
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.ui.Alignment
 
 //import androidx.compose.material3.ExperimentalMaterial3Api
 
@@ -176,7 +177,9 @@ fun DogInformation(
 fun WoofTopAppBar (modifier: Modifier = Modifier) {
     CenterAlignedTopAppBar(
         title = {
-            Row() {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
                 Image(
                     modifier = Modifier
                         .size(dimensionResource(id = R.dimen.image_size))
