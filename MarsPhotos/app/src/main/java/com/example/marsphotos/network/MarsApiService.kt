ae3dacec8d2a0408193e764393f776d9a7601fe9
yurikajoshi01 <joshiyurika2001@gmail.com>
2024-03-22 11:24:58 +0000

    resentencing previous commit (reforit to - retrofit) -> Added a retrofit builder creating an Reforit object to initialize retrofit.

--- MarsPhotos/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ MarsPhotos/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -2,4 +2,5 @@ package com.example.marsphotos.network
 import retrofit2.Retrofit
 
 private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com"
+
 private val retrofit = Retrofit.Builder()
