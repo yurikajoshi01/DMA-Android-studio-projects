b2d7b20b406e141ac19262d1594438d6f96f7e4b
yurikajoshi01 <joshiyurika2001@gmail.com>
2024-03-22 13:48:07 +0000

    added a kotlinx serialization plugin and dependency and replaced Scalar converter dependency with kotlin serialization converter dependency in the build.gradle.kts(:app)

--- MarsPhotos/app/build.gradle.kts
+++ MarsPhotos/app/build.gradle.kts
@@ -19,6 +19,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
 }
 
 android {
@@ -90,7 +91,11 @@ dependencies {
 
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-   // Retrofit with Scalar Converter
-    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
+    // Retrofit with Kotlin serialization Converter
+
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    // Kotlin serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
 
 }
