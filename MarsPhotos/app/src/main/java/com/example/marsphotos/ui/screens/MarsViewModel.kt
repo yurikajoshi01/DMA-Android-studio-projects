47d3731b8dbb1dcbd9b58ccff72f147eaefbbf73
yurikajoshi01 <joshiyurika2001@gmail.com>
2024-03-22 12:59:02 +0000

    updated the marsUiState variable in MarsViewModel, changing type to MarsUiState and default value to be Mars.UiState.Loading.

--- MarsPhotos/app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
+++ MarsPhotos/app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -34,7 +34,7 @@ sealed interface MarsUiState{
 
 class MarsViewModel : ViewModel() {
     /** The mutable State that stores the status of the most recent request */
-    var marsUiState: String by mutableStateOf("")
+    var marsUiState: MarsUiState by mutableStateOf(MarsUiState.Loading)
         private set
 
     /**
