b4b7ac3734479e87db16493a7d1076b0ee04baea
yurikajoshi01 <joshiyurika2001@gmail.com>
2024-05-04 00:06:39 +0100

    added a new dependency io.coil-kt:coil-compose:2.4.0 in build.gradle.kts (:app).

--- MarsPhotos/app/build.gradle.kts
+++ MarsPhotos/app/build.gradle.kts
@@ -80,6 +80,9 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
 
+    // Coil
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
