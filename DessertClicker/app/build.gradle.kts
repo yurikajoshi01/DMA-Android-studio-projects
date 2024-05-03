c351e7a3e00f53aebec55df9c9862724e013fec4
yurikajoshi01 <joshiyurika2001@gmail.com>
2024-05-03 20:39:08 +0100

    added a new lifecycle viewmodel dependency in app build.gradle.kts.

--- DessertClicker/app/build.gradle.kts
+++ DessertClicker/app/build.gradle.kts
@@ -75,6 +75,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
