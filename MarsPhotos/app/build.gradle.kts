4ec6fd0d95638b5dce3eeb9fb971909d1650e039
yurikajoshi01 <joshiyurika2001@gmail.com>
2024-05-04 14:28:14 +0100

    updated dependencies in the build gradle files.

--- MarsPhotos/app/build.gradle.kts
+++ MarsPhotos/app/build.gradle.kts
@@ -19,7 +19,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10"
 }
 
 android {
@@ -80,10 +80,9 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
 
-    // Coil
-    implementation("io.coil-kt:coil-compose:2.4.0")
-
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
@@ -99,6 +98,7 @@ dependencies {
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     // Kotlin serialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
 }
--- MarsPhotos/build.gradle.kts
+++ MarsPhotos/build.gradle.kts
@@ -18,7 +18,8 @@
 
 plugins {
     id("com.android.application") version "8.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.library") version "8.1.3" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
 
 tasks.register("clean", Delete::class) {
